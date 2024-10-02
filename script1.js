894a153356c3f76252e3f9ba8c5d728047f01b0e
Ikram205 <149678364+Ikram205@users.noreply.github.com>
2024-10-02 16:12:17 +0530

    commit7

--- script1.js
+++ script1.js
@@ -6,3 +6,25 @@ var swiper = new Swiper(".home", {
       prevEl: ".swiper-button-prev",
     },
 });
+function show_list()
+{
+  var device = document.getElementById("list");
+
+
+  if (device.style.display == "block")
+  {
+    device.style.display == "none";
+  }
+  else
+  {
+    device.style.display == "block";
+    }
+}
+window.onclick = function (event)
+{
+  if (!event.target.matches('.menu-icon'))
+  {
+    document.getElementById('list')
+      .style.display = "none";
+  }
+}
