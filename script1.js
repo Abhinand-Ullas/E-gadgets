39b125daa1ecefd2ff6931b90beca6fa1e70e8c6
Ikram205 <149678364+Ikram205@users.noreply.github.com>
2024-10-17 12:40:48 +0530

    commit20

--- script1.js
+++ script1.js
@@ -6,16 +6,3 @@ var swiper = new Swiper(".home", {
       prevEl: ".swiper-button-prev",
     },
 });
-function show_list() {
-  var device = document.getElementById("list");
-  device.classList.toggle("visible");  // Toggle visibility
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.menu-icon')) {
-    var dropdown = document.getElementById("list");
-    if (dropdown.classList.contains('visible')) {
-      dropdown.classList.remove('visible');
-    }
-  }
-}
